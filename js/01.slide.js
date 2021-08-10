6e4b5da1be852c06d0e2a5d6d2a7fddf18fd65f1
1binibin <namwonbin@gmail.com>
2021-08-10 15:03:38 +0900

    답안지

--- js/01.slide.js
+++ js/01.slide.js
@@ -1,9 +1,9 @@
 /*************** global init **************/
 var $wrapper = $('.wrapper1');
 var $slide = $wrapper.find('.slide-wrap');
-var $pager = $wrapper.find('.wrapper .pager');
-var $btPrev = $wrapper.find('bt-prev');
-var $btNext = $wrapper.find('bt-next');
+var $pager = $wrapper.find('.pager');
+var $btPrev = $wrapper.find('.bt-prev');
+var $btNext = $wrapper.find('.bt-next');
 var interval, n = 0, speed = 2000, aniSpeed = 500, cnt = $slide.find('.slide').length;
 
 /************** user function *************/
@@ -23,33 +23,33 @@ function onLeave() {
 }
 
 function onAni() {
-    if(n === cnt - 1) {
-        $slide.css('left', 0);
-        n = 1;
-    }
-    else n++;
-    ani($slide, $pager, aniSpeed, n);
+	if(n === cnt - 1) {
+		$slide.css('left', 0);
+		n = 1;
+	}
+	else n++;
+	ani($slide, $pager, aniSpeed, n);
 }
 
 function onPagerClick() {
 	n = $(this).index();
-    ani($slide, $pager, aniSpeed, n)
+	ani($slide, $pager, aniSpeed, n);
 }
 
 function onPrev() {
-    if(n === 0){
-        $slide.css('left', -(cnt - 1)*100 +'%');
-    }
-    n = (n === 0) ? cnt -2 : n - 1;
-    ani($slide, $pager, aniSpeed, n)
+	if(n === 0) {
+		$slide.css('left', -(cnt - 1)*100 +'%');
+	}
+	n = (n === 0) ? cnt - 2 : n - 1;
+	ani($slide, $pager, aniSpeed, n);
 }
 
 function onNext() {
-    if(n === cnt - 1) {
-        $slide.css('left', 0);
-    }
-    n = (n === cnt -1) ? 1 : n + 1;
-    ani($slide, $pager, aniSpeed, n)
+	if(n === cnt - 1) {
+		$slide.css('left', 0);
+	}
+	n = (n === cnt - 1) ? 1 : n + 1;
+	ani($slide, $pager, aniSpeed, n);
 }
 
 /*************** event init ***************/
