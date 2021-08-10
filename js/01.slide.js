0e49695249723c07cfcb4bc48da6083792ca98da
1binibin <namwonbin@gmail.com>
2021-08-10 13:23:07 +0900

    수정

--- js/01.slide.js
+++ js/01.slide.js
@@ -1,45 +1,46 @@
-/*************** global init *****************/
+/*************** global init **************/
 var $wrapper = $('.wrapper1');
 var $slide = $wrapper.find('.slide-wrap');
-var $pager = $wrapper.find('.wrapper1 .pager');
-var interval, n = 1,speed = 2000, aniSpeed = 500, cnt = $slide.find('.slide').lenght;
+var $pager = $wrapper.find('.wrapper .pager');
+var interval, n = 1, speed = 2000, aniSpeed = 500, cnt = $slide.find('.slide').length;
 
-/*************** user function  *****************/
+/************** user function *************/
 function chgPager(el, idx) {
-    $(el).removeClass('active');
-    $(el).eq(idx).addClass('active');
+	$(el).removeClass('active');
+	$(el).eq(idx).addClass('active');
 }
 
-/*************** event callback *****************/
+/************** event callback ************/
 function onAni() {
-    $slide.stop().animate({'left': -n * 100+'%'}, aniSpeed, function() {
-        if(n === cnt -1) {
-            $slide.css('left', 0);
-            chgPager($pager, 0);
-            n = 1;
-        }
-        else {
-            chgPager($pager, n++);
-        }
-    });
+	$slide.stop().animate({'left': -n * 100+'%'}, aniSpeed, function() {
+		if(n === cnt - 1) {
+			$slide.css('left', 0);
+			chgPager($pager, 0);
+			n = 1;
+		}
+		else {
+			chgPager($pager, n++);
+		}
+	});
 }
 
 function onEnter() {
-    clearInterval(interval);
+	clearInterval(interval);
 }
 
 function onLeave() {
-    interval = setInterval(onAni, speed);
+	interval = setInterval(onAni, speed);
 }
 
 function onPagerClick() {
-    n = $(this).index();
-    onAni();
+	n = $(this).index();
+	onAni();
 }
-/*************** event init *****************/
+
+/*************** event init ***************/
 interval = setInterval(onAni, speed);
-$wrapper.mouseenter(onEnter).mouseleave(onLeave)
+$wrapper.mouseenter(onEnter).mouseleave(onLeave);
 $pager.click(onPagerClick);
 
-/*************** start init *****************/
+/*************** start init ***************/
 
