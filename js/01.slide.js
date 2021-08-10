6c472b5cc06f19a23dac06ccd615931f51dbaac1
1binibin <namwonbin@gmail.com>
2021-08-10 13:01:26 +0900

    pager 및 사진바뀌기

--- js/01.slide.js
+++ js/01.slide.js
@@ -1,19 +1,38 @@
 /*************** global init *****************/
 var $slide = $('.wrapper1 .slide-wrap');
-var interval, n = 1, cnt = $slide.find('.slide').lenght;
+var $pager = $('.wrapper1 .pager');
+var interval, n = 1,speed = 2000, aniSpeed = 500, cnt = $slide.find('.slide').lenght;
 
 /*************** user function  *****************/
-
+function chgPager(el, idx) {
+    $(el).removeClass('active');
+    $(el).eq(idx).addClass('active');
+}
 
 /*************** event callback *****************/
 function onSet() {
-    $slide.stop().animate({'left': -(n * 100)+'%'}, 1000, function () {
-        ++n;
+    $slide.stop().animate({'left': -n * 100+'%'}, aniSpeed, function () {
+        if(n === cnt -1) {
+            $slide.css('left', 0);
+            chgPager($pager, 0);
+            n = 1;
+        }
+        else {
+            chgPager($pager, n++);
+        }
     });
 }
 
+function onEnter() {
+    clearInterval(interval);
+}
+
+function onLeave() {
+    interval = setInterval(onSet, speed);
+}
 /*************** event init *****************/
-interval = setInterval(onSet, 3000);
+interval = setInterval(onSet, speed);
+$slide.mouseenter(onEnter).mouseleave(onLeave)
 
 /*************** start init *****************/
 
