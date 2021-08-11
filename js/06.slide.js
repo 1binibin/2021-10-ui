90e8b7e0fc676ed9d626ae560c60f6a921e88cda
1binibin <namwonbin@gmail.com>
2021-08-11 16:18:30 +0900

    pager button전

--- js/06.slide.js
+++ js/06.slide.js
@@ -1,5 +1,18 @@
-var slide = new Slide('.slide-wrapper', {
-	autoPlay: false,
-	effect: 'horizontal',
+var slide = new Slide('#slide1', {
+	autoPlay: true,
+	effect: 'fade',
+    autoPlaySpeed: 1000,
+	speed: 500
+});
+var slide2 = new Slide('#slide2', {
+	autoPlay: true,
+	effect: 'fade',
+    autoPlaySpeed: 2000,
+	speed: 500
+});
+var slide3 = new Slide('#slide3', {
+	autoPlay: true,
+	effect: 'fade',
+    autoPlaySpeed: 3000,
 	speed: 500
 });
