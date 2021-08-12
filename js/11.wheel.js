a5048ee4e2cdd92ddc43e9cb78a229341823452b
1binibin <namwonbin@gmail.com>
2021-08-12 13:19:22 +0900

    wheel ~ing

--- js/11.wheel.js
+++ js/11.wheel.js
@@ -1,3 +1,9 @@
+/**
+ *  Event(JS) Bubbling
+ *  event.preventDefault();     event 막기 / wheel에 적용시:사용 X 막아놈 --> jQuery안됨 JS 사용!
+ *  event.stopPropagation();    적용된것만 나옴
+ */
+
 /*************** global init *****************/
 
 
@@ -6,11 +12,31 @@
 
 
 /*************** event callback *****************/
+function onWheel(e) {
+    e.preventDefault();
+}
 
+function onWrapperClick(e) {
+    console.log('wrapper');
+}
 
+function onContentWrapClick(e) {
+    e.stopPropagation();
+    console.log('content-wrap');
+}
+
+function onBoxClick(e) {
+    // e.preventDefault();
+    e.stopPropagation();
+    console.log('box');
+}
 
 /*************** event init *****************/
+window.addEventListener('wheel', onWheel, {passive: false});
 
+$('.wrapper').click(onWrapperClick);
+$('.wrapper .content-wrap').click(onContentWrapClick);
+$('.wrapper .box').click(onBoxClick);
 
 
 /*************** start init *****************/
